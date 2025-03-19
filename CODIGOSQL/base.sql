622cfa2cc35f320d3a9884f76dc62329bc997ee9
raulcsa <raulcasadosanandres1@gmail.com>
2025-03-19 20:40:01 +0100

    Update base.sql

--- CODIGOSQL/base.sql
+++ CODIGOSQL/base.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE tfgs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    fecha DATE DEFAULT NULL,
+    nota INT CHECK (nota BETWEEN 1 AND 10),
+    resumen TEXT,
+    palabras_clave VARCHAR(255) DEFAULT NULL,
+    integrantes TEXT,
+    CONSTRAINT chk_fecha CHECK (fecha REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
+);
+INSERT INTO tfgs (titulo, fecha, nota, resumen, palabras_clave, integrantes) 
+VALUES
+(),
